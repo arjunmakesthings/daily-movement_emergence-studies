666ce1e634945b63ec9b93eb0d45c4bb499f2507
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2026-06-11 15:57:48 -0400

    temp.

--- main/main.js
+++ main/main.js
@@ -8,6 +8,9 @@ function setup() {
 	// createCanvas(1000, 562); //in 16:9 aspect ratio.
 	createCanvas(800, 800); //square.
 
+	//p5 stuff; globally; once:
+	noStroke(); 
+
 	world = new World(); 
 }
 
@@ -17,14 +20,14 @@ function draw() {
 
 class World {
 	constructor(){
-		this.time;
+		this.time = 0;
 		this.beings = []; 
 	}
 	exist(){
-		background(255, 0, 0);
-		this.keep_time(); 
+		background(255);
+		this.keep_time();
 
-		console.log(this.time);
+		
 	}
 	keep_time(){
 		this.time = Math.floor(frameCount/60 % 24); 
@@ -32,7 +35,15 @@ class World {
 }
 
 class Being {
-	constructor(){
-		this.pos; 
+	constructor(x,y){
+		this.pos = createVector(this.x, this.y); 
+		this.age = 0; 
+	}
+	exist(){
+		this.body(); 
+	}
+	body(){
+		fill (0); 
+		circle (this.x, this.y, mass); 
 	}
 }
