2c762f8c885d42f80cc1ab57158bee1df0de38ce
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2026-06-11 17:27:49 -0400

    interim.

--- main/main.js
+++ main/main.js
@@ -27,23 +27,40 @@ class World {
 		background(255);
 		this.keep_time();
 
-		
+		this.make_and_remove(); //or birth & death.
+
+		for (let being of this.beings){
+			being.exist(); 
+		}
 	}
 	keep_time(){
 		this.time = Math.floor(frameCount/60 % 24); 
 	}
-}
+	make_and_remove(){
+		/*
+		death criteria: 
+		-  
+		*/
+	}
 
 class Being {
 	constructor(x,y){
-		this.pos = createVector(this.x, this.y); 
-		this.age = 0; 
+		this.pos = createVector(x, y); 
+		this.curr_age = 0; 
+		this.mass = 20; 
 	}
 	exist(){
-		this.body(); 
+		this.body();
+		this.age(); 
 	}
 	body(){
 		fill (0); 
-		circle (this.x, this.y, mass); 
+		circle (this.pos.x, this.pos.y, this.mass); 
+	}
+	age(){
+			
+	}
+	move(){
+			
 	}
 }
---
+++ trials/test.js
@@ -0,0 +1,10 @@
+function something(n){
+	 fn2(); 
+}
+
+function fn2(){
+	console.log(n); 
+}
+
+something(2);
+
