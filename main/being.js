b1456c0e64876c6b0762907852d00f6df9f2a07c
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2026-06-22 17:44:53 -0400

    temp

--- main/being.js
+++ main/being.js
@@ -1,15 +1,21 @@
 class Being {
   constructor(x, y) {
-    this.pos = createVector(x, y);
+    this.pos = createVector(x, y); 
     this.curr_age = 0;
     this.mass = 10;
-    this.energy = 100;
+    this.energy = 0;
 
-    this.schedule = new Array(24);
+    this.velocity = createVector(0,0); 
+
+    this.schedule = this.get_schedule();
+  }
+  get_schedule(){
+
   }
   exist() {
     this.body();
     this.age();
+    this.move();
   }
   body() {
     fill(0);
@@ -22,32 +28,39 @@ class Being {
 
       //as age increases, energy decreases; while mass increases.
       this.mass = this.get_mass();
-      console.log(this.curr_age, this.mass);
+      this.energy = this.get_energy(); 
+
+      console.log(this.curr_age, this.mass, this.energy); 
     }
   }
   get_mass() {
     //for a given current age, calculate mass.
 
+    //mass is an asymptotic-exponential-growth graph.
+
     const a = 50; //max.
     const b = 18; //in how many steps is max achieved.
     const base = 10;
 
-    return a * (1-Math.exp(-5 * (this.curr_age / b))) / (1-Math.exp(-5)); 
-
-    //return (a * (1 - Math.exp(-this.curr_age / b))) / (1 - Math.exp(-1));
-
-    /*
-    const a = 50;
-const maxT = 18;
-
-let t = this.curr_age / maxT;
-t = Math.min(Math.max(t, 0), 1);
-
-return a * (1 - Math.exp(-5 * t)) / (1 - Math.exp(-5));
-    */
+    return (a * (1 - Math.exp(-5 * (this.curr_age / b)))) / (1 - Math.exp(-5));
+  }
+  get_energy(){
+    //energy is like a bell curve.
+    return 0.03 * this.curr_age * this.curr_age * Math.exp(-this.curr_age / 18);
   }
   move() {
-    //beings move according to a schedule.
+    //beings move according to a schedule, with a speed that is a factor of their masses.
+    this.pos.add(this.velocity); 
+
+    this.constrain();
+  }
+  constrain(){
+    if (this.pos.x+this.mass/2 >= width || this.pos.x - this.mass/2 <= 0){
+      this.velocity.x*=-1;
+    }
+    if (this.pos.y + this.mass/2 > height || this.pos.y - this.mass/2 < 0) {
+      this.velocity.y *= -1;
+    }
   }
   reproduce() {
     //beings in close proximity to each other give rise to another being.
